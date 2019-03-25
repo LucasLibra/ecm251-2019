b8b0c95ae03655adb1465ce88947f6a044e6ccfe
LUCAS  NEGRESIOLO LIBORIO <17.01165-5@maua.br>
2019-03-25 12:49:01 -0300

    Projeto das Compras

---
+++ pratica03/projetoCliente/src/com/company/Cliente.java
@@ -0,0 +1,52 @@
+package com.company;
+
+public class Cliente {
+    private String nome;
+    private String ra;
+    private boolean funcionario;
+    private boolean visitante;
+
+
+    public void comprar() {
+
+    }
+
+    public void acompanhar() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getRa() {
+        return ra;
+    }
+
+    public boolean isFuncionario() {
+        return funcionario;
+    }
+
+    public boolean isVisitante() {
+        return visitante;
+    }
+
+    public Cliente(String nome) {
+        this.nome = nome;
+        this.visitante = true;
+    }
+
+    public Cliente(String nome, boolean funcionario){
+        this.nome = nome;
+        if(funcionario)
+            this.funcionario = funcionario;
+        else
+            visitante = true;
+    }
+    public Cliente(String nome, String ra){
+        this.nome = nome;
+        this.ra = ra;
+    }
+
+
+}
